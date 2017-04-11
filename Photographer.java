6eb43bb6734085fe7422494d24d0538d5a3b03ae
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-11 15:21:59 +0100

    more setup added

--- Photographer.java
+++ Photographer.java
@@ -2,5 +2,11 @@ import java.util.*;
 
 public class Photographer {
   private String name;
-  private ArrayList<Cameras> cameras;
+  private ArrayList<Camera> cameras;
+
+  public Photographer(String name){
+    this.name = name;
+    this.cameras = new ArrayList<Camera>();
+  }
+
 }
