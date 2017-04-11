2d83e79121c03b0fe0223b05a32965c1c8bd7242
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-11 16:06:51 +0100

    canaddcamera test added

--- PhotographerTest.java
+++ PhotographerTest.java
@@ -3,15 +3,25 @@ import org.junit.*;
 
 public class PhotographerTest {
   Photographer photographer;
+  AnalogCamera analogcamera;
+  DigitalCamera digitalcamera;
 
   @Before
   public void before(){
   photographer = new Photographer("Allegra");
+  analogcamera = new AnalogCamera("Polaroid");
+  digitalcamera = new DigitalCamera("Canon");
   }
 
   @Test
   public void hasName(){
     assertEquals("Allegra", photographer.getName());
+  }
+
+  @Test
+  public void canAddCamera(){
+    photographer.addCamera();
+    assertEquals("Polaroid", photographer.getName());
   }
 
 
